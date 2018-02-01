7fb51d75d1254bb4f115aeaa805755469b274da0
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-02-01 15:13:20 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-  ddd
+  resouces :plans
+  resouces :places
 end
