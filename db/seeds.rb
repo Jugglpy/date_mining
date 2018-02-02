5906f5fa5fcadd3215415114ccc7c501ec80e635
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-02-02 15:15:59 +0900

    make seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-  Place.create(name: 'スカイツリー')
-  Place.create(name: '東京タワー')
-  Place.create(name: '浅草寺')
+  Plan.create(name: '表参道ぶらりデートプラン', cost: "30000", time: "2018-01-10", season: "春")
+  Plan.create(name: '表参道ぶらりデートプラン2', cost: "40000", time: "2018-01-11", season: "夏")
 
-  5.times do |no|
-    Order.create(plan_id: no, order: no, place_id: no)
-  end
+  Place.create(name: 'アップル表参道', latitude: "35.666217", longtitude: "139.711096")
+  Place.create(name: '竹下通り', latitude: "35.683358", longtitude: "139.701045")
+  Place.create(name: '代官山', latitude: "35.003771", longtitude: "135.88208")
+
+  Order.create(plan_id: 1, place_id: 3, order: 1)
+  Order.create(plan_id: 1, place_id: 2, order: 2)
+  Order.create(plan_id: 1, place_id: 1, order: 3)
+
+  Order.create(plan_id: 2, place_id: 1, order: 3)
+  Order.create(plan_id: 2, place_id: 1, order: 3)
+  Order.create(plan_id: 2, place_id: 1, order: 3)
