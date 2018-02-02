6b04a56efef7efb28cc6baa68368601c871ffa73
齋藤成志 <saisige1208@gmail.com>
2018-02-02 14:30:12 +0900

    edit show_view & create column
    to create show_view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202044709) do
+ActiveRecord::Schema.define(version: 20180202052629) do
 
   create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "plan_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180202044709) do
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
     t.text     "image",      limit: 65535
+    t.text     "explain",    limit: 65535
   end
 
   create_table "plans", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
